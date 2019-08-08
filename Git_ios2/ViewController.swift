881b9a14971141dbbc9123a9431b7052bde9fe88
Nura-ainee Yako <tanylap_sath@hotmail.com>
2019-08-08 14:13:21 +0700

    Initial Commit

---
+++ Git_ios2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git_ios2
+//
+//  Created by Nura-ainee Yako on 8/8/2562 BE.
+//  Copyright © 2562 Nura-ainee Yako. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Git_ios2Tests/Git_ios2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Git_ios2Tests.swift
+//  Git_ios2Tests
+//
+//  Created by Nura-ainee Yako on 8/8/2562 BE.
+//  Copyright © 2562 Nura-ainee Yako. All rights reserved.
+//
+
+import XCTest
+@testable import Git_ios2
+
+class Git_ios2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
